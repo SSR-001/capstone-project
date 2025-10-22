f83aefca579ba6b4d3a1273702a54a9d74f8bb29
SSR-001 <ssrichardson1010@gmail.com>
2025-10-22 17:26:36 +0100

    debug efforts

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from my_website import views as my_website_views  #dont feel good about this
+# from my_website import views as my_website_views  #dont feel good about this
+# from my_website import urls as my_website_urls #dont feel good about this
 
 
 urlpatterns = [
