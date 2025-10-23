7bf3d617e3411faa224af36f73085f748360b153
SSR-001 <ssrichardson1010@gmail.com>
2025-10-23 12:17:25 +0100

    start writing ArticleList class in views.

--- my_website/admin.py
+++ my_website/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import ArticleModel
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 
-admin.site.register(ArticleModel)
+# admin.site.register(ArticleModel) #told to delete this
+
+#does this even work?:
+# @admin.register(ArticleModel) #is this the best name?
+# class ArticleModelAdmin(SummernoteModelAdmin):  #is this the best name?
+#     #field names in model:
+#     # title/, article_body/, writer/, date written/, approved/, slug/, updated on/, blurb/, order/
+#     list_display = ('writer', 'slug', 'date_written')
+#     search_fields = ['writer', 'article_body']
+#     list_filter = ('approved', 'date_written')
+#     prepoulated_fields = {'slug': ('title',)}
+#     summernote_fields = ('content',)
+    
+    
+    
--- my_website/views.py
+++ my_website/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home_page_view(request):
     return HttpResponse("Hello, World!")
+
+class ArticleList(generic.ListView):
+    pass
