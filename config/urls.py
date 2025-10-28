1f2630069eca97d63743f4e9708cf24be411eca9
SSR-001 <ssrichardson1010@gmail.com>
2025-10-28 00:17:05 +0000

    add allauth code, login, logout register pages

--- config/urls.py
+++ config/urls.py
@@ -23,5 +23,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('accounts/', include("allauth.urls")),
     path("", include("my_website.urls"))
 ]
