93e7a675701bfe5ed12a2d11edcd1715aa441a25
SSR-001 <ssrichardson1010@gmail.com>
2025-11-05 07:44:50 +0000

    delete model fields slug and blurb

--- my_website/admin.py
+++ my_website/admin.py
@@ -8,10 +8,8 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Article)
 class ArticleAdmin(SummernoteModelAdmin):
     # field names in the model:
-    # title/, article_body/, writer/, date written/, approved/, slug/,
-    # updated on/, blurb/, order/
+    # title, article_body, writer, date_written, ready_for_approval, approved, when_updated
     list_display = ('title', 'writer', 'ready_for_approval', 'approved',)
     search_fields = ['article_body']
     list_filter = ('approved', 'date_written')
-    prepoulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
--- my_website/models.py
+++ my_website/models.py
@@ -10,14 +10,12 @@ REQUESTAPPROVAL = ((0, "Not Ready For Approval"), (1, "Ready For Approval"))
 
 class Article (models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
     article_body = models.TextField()
     date_written = models.DateTimeField(auto_now_add=True)
     ready_for_approval = models.IntegerField(choices=REQUESTAPPROVAL,
         default=0)
     approved = models.IntegerField(choices=STATUS, default=0)
-    blurb = models.TextField(blank=True)
     when_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
